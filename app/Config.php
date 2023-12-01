d34a627208f7a0eba5bc17f0d2cf7a078ccdddcd
pabaco <130976284+PabacoDAW@users.noreply.github.com>
2023-12-01 13:49:46 +0100

    Se añade Config.php

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos52023";
+     static public $mvc_bd_usuario  = "mycasto710a";
+     static public $mvc_bd_clave    = "e1bz6E4h";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
